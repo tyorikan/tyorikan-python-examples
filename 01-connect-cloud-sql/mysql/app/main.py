661d61c22d29aea5d234dc70b6b456926fff7b51
tyorikan <tyorikan@gmail.com>
2023-03-14 13:34:35 +0900

    fix sqlalchemy

--- 01-connect-cloud-sql/mysql/app/main.py
+++ 01-connect-cloud-sql/mysql/app/main.py
@@ -3,82 +3,30 @@ import os
 from fastapi import FastAPI
 import sqlalchemy
 
+from app.connect_connector import connect_with_connector
+from app.connect_connector_auto_iam_authn import connect_with_connector_auto_iam_authn
+from app.connect_tcp import connect_tcp_socket
+from app.connect_unix import connect_unix_socket
+
 app = FastAPI()
 
 
-def init_connection_engine():
-    db_config = {
-        "pool_size": 5,
-        "max_overflow": 2,
-        "pool_timeout": 30,  # 30 seconds
-        "pool_recycle": 1800,  # 30 minutes
-    }
+def init_connection_pool() -> sqlalchemy.engine.base.Engine:
+    if os.environ.get("INSTANCE_HOST"):
+        return connect_tcp_socket()
 
-    if os.environ.get("DB_HOST"):
-        return init_tcp_connection_engine(db_config)
-    return init_unix_connection_engine(db_config)
+    if os.environ.get("INSTANCE_UNIX_SOCKET"):
+        return connect_unix_socket()
 
+    if os.environ.get("INSTANCE_CONNECTION_NAME"):
+        return connect_with_connector_auto_iam_authn() if os.environ.get("DB_IAM_USER") else connect_with_connector()
 
-def init_tcp_connection_engine(db_config):
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
-    db_host = os.environ["DB_HOST"]
-
-    host_args = db_host.split(":")
-    if len(host_args) == 1:
-        db_hostname = db_host
-        db_port = os.environ["DB_PORT"]
-    elif len(host_args) == 2:
-        db_hostname, db_port = host_args[0], int(host_args[1])
-
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,  # e.g. "my-database-user"
-            password=db_pass,  # e.g. "my-database-password"
-            host=db_hostname,  # e.g. "127.0.0.1"
-            port=db_port,  # e.g. 3306
-            database=db_name,  # e.g. "my-database-name"
-        ),
-        **db_config
+    raise ValueError(
+        "Missing database connection type. Please define one of INSTANCE_HOST, INSTANCE_UNIX_SOCKET, or INSTANCE_CONNECTION_NAME"
     )
 
-    return pool
 
-
-def init_unix_connection_engine(db_config):
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
-    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
-
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,  # e.g. "my-database-user"
-            password=db_pass,  # e.g. "my-database-password"
-            database=db_name,  # e.g. "my-database-name"
-            query={
-                "unix_socket": "{}/{}".format(
-                    db_socket_dir, instance_connection_name  # e.g. "/cloudsql"
-                )  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-            },
-        ),
-        **db_config
-    )
-
-    return pool
-
-
-db = None
-
-
-@app.on_event("startup")
-def create_tables():
-    global db
-    db = db or init_connection_engine()
+def migrate_db(db: sqlalchemy.engine.base.Engine) -> None:
     sql = """
         CREATE TABLE IF NOT EXISTS users (
             id INT PRIMARY KEY,
@@ -86,10 +34,22 @@ def create_tables():
             status TINYINT NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         ) ENGINE=INNODB;
-        INSERT IGNORE INTO users VALUES (12345, "店長", 0, now());
     """
+    sql2 = 'INSERT IGNORE INTO users VALUES (12345, "店長", 0, now());'
     with db.connect() as conn:
-        conn.execute(sql)
+        conn.execute(sqlalchemy.text(sql))
+        conn.execute(sqlalchemy.text(sql2))
+        conn.commit()
+
+
+db = None
+
+
+@app.on_event("startup")
+def init_db() -> sqlalchemy.engine.base.Engine:
+    global db
+    db = init_connection_pool()
+    migrate_db(db)
 
 
 @app.get("/")
