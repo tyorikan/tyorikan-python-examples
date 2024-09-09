fa7733a063120af92054e4ce01748f84c9db5a82
Takayuki Yorikane <tyorikan@gmail.com>
2024-09-09 14:38:22 +0900

    Add history to the prompt

--- 08-grpc/grpc_server.py
+++ 08-grpc/grpc_server.py
@@ -8,6 +8,8 @@ import genai_pb2_grpc
 import vertexai
 from vertexai.generative_models import (
     GenerativeModel,
+    Content,
+    Part,
     HarmCategory,
     HarmBlockThreshold,
 )
@@ -16,7 +18,7 @@ import os
 vertexai.init(project=os.environ["GOOGLE_CLOUD_PROJECT"], location="asia-northeast1")
 model = GenerativeModel(
     model_name="gemini-1.5-flash-001",
-    system_instruction="あなたは日本のトップ芸人で最高に面白いです。たまに面白い冗談を入れつつ簡潔に返答してください。",
+    system_instruction="あなたは最高に明るく、ポジティブで面白い人です。たまに面白いことを話しつつ簡潔に会話してください。",
     safety_settings={
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_ONLY_HIGH,
@@ -33,17 +35,32 @@ generation_config = {
 
 
 class GenAI(genai_pb2_grpc.GeneratorServicer):
+    def __init__(self):
+        super().__init__()
+        self.history = []
+
     def GenAIResponse(self, request, context):
         try:
+            # TODO DB から取得して append する
+            self.history.append(
+                Content(role="user", parts=[Part.from_text(request.prompt)])
+            )
+
             responses = model.generate_content(
-                [request.prompt],
+                contents=self.history,
                 generation_config=generation_config,
                 stream=True,
             )
 
             # Sending actual response.
+            fulltext = ""
             for response in responses:
                 yield genai_pb2.MessageResponse(message=response.text)
+                fulltext += response.text
+
+            # TODO DB に追加する
+            self.history.append(Content(role="model", parts=[Part.from_text(fulltext)]))
+
         except ValueError as e:
             yield genai_pb2.MessageResponse(
                 message="LLM からのデータ取得に失敗しました - " + str(e)
