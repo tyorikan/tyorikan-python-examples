6a822b565b5dae45f72248c3453d9a8db1576dbe
Takayuki Yorikane <tyorikan@gmail.com>
2024-10-17 13:47:44 +0900

    Add sidecar sample

---
+++ 12-api-gateway-sidecar/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/")
+def write_root():
+    return {"Hello": "World"}
